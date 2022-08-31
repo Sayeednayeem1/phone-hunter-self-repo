798d147969a4fe68a79addebb81454283cb083b5
Sayeednayeem1 <sayeedn850@gmail.com>
2022-08-31 16:29:11 +0600

    fetched the link

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loadPhones = async() =>{
+    const url = ` https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones =>{
+    console.log(phones);
+    const phonesContainer = document.getElementById('phone-container');
+    phonesContainer.classList.add('col');
+    
+}
+
+loadPhones();
