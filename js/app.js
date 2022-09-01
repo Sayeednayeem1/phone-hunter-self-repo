3484828baad769d3406dbfda4828c9f4fc316984
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-01 12:26:11 +0600

    added show details section and that concludes the page adios

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // load phone field
-const loadPhones = async(searchText, dataLimit) =>{
+const loadPhones = async (searchText, dataLimit) => {
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
@@ -8,30 +8,30 @@ const loadPhones = async(searchText, dataLimit) =>{
 }
 
 // display field
-const displayPhones = (phones, dataLimit) =>{
+const displayPhones = (phones, dataLimit) => {
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
     // diplay 5 phones only
     const showAll = document.getElementById('show-all');
-    if(dataLimit && phones.length > 5){
+    if (dataLimit && phones.length > 5) {
         phones = phones.slice(0, 5);
-        showAll.classList.remove('d-none');  
+        showAll.classList.remove('d-none');
     }
-    else{
+    else {
         showAll.classList.add('d-none');
     }
-    
+
 
     // display no phone found
     const noPhone = document.getElementById('no-found-message');
-    if(phones.length === 0){
+    if (phones.length === 0) {
         noPhone.classList.remove('d-none');
     }
-    else{
+    else {
         noPhone.classList.add('d-none');
     }
-    phones.forEach(phone =>{
+    phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
@@ -40,16 +40,17 @@ const displayPhones = (phones, dataLimit) =>{
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
         </div>
         </div>
         `;
         phonesContainer.appendChild(phoneDiv);
     });
     toggleSpinner(false);
-    
+
 }
 
-const processSearch = (dataLimit) =>{
+const processSearch = (dataLimit) => {
     toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -57,24 +58,50 @@ const processSearch = (dataLimit) =>{
 }
 
 // button click field
-document.getElementById('btn-search').addEventListener('click', function(){
+document.getElementById('btn-search').addEventListener('click', function () {
     processSearch(5)
 })
 
-const toggleSpinner = isLoading =>{
+// search field enter key handler
+
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    if(e.key === "Enter"){
+        processSearch(5);
+    }
+})
+
+const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(isLoading){
+    if (isLoading) {
         loaderSection.classList.remove("d-none");
     }
-    else{
+    else {
         loaderSection.classList.add("d-none");
     }
 }
 
 // not the best way to load show all
 
-document.getElementById('btn-show-all').addEventListener('click', function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 })
 
-loadPhones("iphone");
+const loadPhoneDetails = async id =>{
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('show-details');
+    phoneDetails.innerHTML = `
+    
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : "no release date found"}</p>
+    `;
+}
+
+
+loadPhones('samsung');
